604c181e786e7d9b260fe77d450d053398f95502
BriggitteDr <geraldinedrouet1993@hotmail.com>
2023-07-03 20:00:56 -0500

    arreglos menores

---
+++ app/src/main/java/com/example/elperlanegra/ui/perfil/PerfilFragment.java
@@ -0,0 +1,27 @@
+package com.example.elperlanegra.ui.perfil;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.elperlanegra.R;
+
+
+public class PerfilFragment extends Fragment {
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_perfil, container, false);
+        return root;
+    }
+
+
+}
