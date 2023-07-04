88f1572c79be579435b5befbdae750b1ccbdde8e
BriggitteDr <geraldinedrouet1993@hotmail.com>
2023-07-03 20:51:44 -0500

    Diseño y funcionalidad de AcercaActivity listo

---
+++ app/src/main/java/com/example/elperlanegra/AcercaActivity.java
@@ -0,0 +1,34 @@
+package com.example.elperlanegra;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import java.util.Objects;
+
+public class AcercaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca);
+
+        /////////////FLECHA HACIA ATRAS
+        Toolbar toolbar = findViewById(R.id.tb_acerca);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
