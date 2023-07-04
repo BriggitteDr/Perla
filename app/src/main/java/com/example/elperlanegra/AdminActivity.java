a18a1149fbbc14005afde67ad58777d5a965a027
BriggitteDr <geraldinedrouet1993@hotmail.com>
2023-07-03 23:37:06 -0500

    Diseño de AdminActivity listo, funcionalidad proximamente en desarrollo

--- app/src/main/java/com/example/elperlanegra/AdminActivity.java
+++ app/src/main/java/com/example/elperlanegra/AdminActivity.java
@@ -1,8 +1,13 @@
 package com.example.elperlanegra;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import java.util.Objects;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        /////////////FLECHA HACIA ATRAS
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Toolbar toolbar = findViewById(R.id.tb_admin);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
